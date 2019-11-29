1ce780ab80b517e2646388e093c1f7b180137c00
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-11-29 03:17:57 +0000

    Add sum_of_two.cpp

---
+++ sum_of_two.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum: " << (a + b) << endl;
+    return 0;
+}
