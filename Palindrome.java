f530250d3add26489528a20321e09c9b2e86a816
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-12-03 01:43:49 +0000

    Add Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scanner.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
