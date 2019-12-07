3c026c65f9948ba50e5b1f320955ebb5d966862a
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-12-07 05:36:08 +0000

    Add EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even.");
+        } else {
+            System.out.println(num + " is Odd.");
+        }
+    }
+}
