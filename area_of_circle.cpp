681a4f5d5e0d9acfd9dd676497d2e61b6df21f4d
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-12-10 10:00:41 +0000

    Add area_of_circle.cpp

---
+++ area_of_circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float radius;
+    cout << "Enter radius: ";
+    cin >> radius;
+    cout << "Area of circle: " << (3.14159 * radius * radius) << endl;
+    return 0;
+}
