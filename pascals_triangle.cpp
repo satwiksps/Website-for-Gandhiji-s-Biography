07ec5808d1ba3f09bb39f76d7e5ed65549cc09f3
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-12-19 04:27:31 +0000

    Add pascals_triangle.cpp

---
+++ pascals_triangle.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void generatePascalsTriangle(int n) {
+    vector<vector<int>> triangle(n);
+
+    for (int i = 0; i < n; i++) {
+        triangle[i].resize(i + 1);
+        triangle[i][0] = triangle[i][i] = 1;
+
+        for (int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << triangle[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    generatePascalsTriangle(n);
+    return 0;
+}
