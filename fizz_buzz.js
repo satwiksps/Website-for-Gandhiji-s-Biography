bb155e53819ebf0cefe3a8d1c26b394fb52b7f3f
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-12-25 12:37:23 +0000

    Add fizz_buzz.js

---
+++ fizz_buzz.js
@@ -0,0 +1,17 @@
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+const num = parseInt(prompt("Enter a number: "), 10);
+fizzBuzz(num);
