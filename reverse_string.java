bdc728ed1244244566b80f6a6a3710b85c7c76e1
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2019-12-26 13:44:15 +0000

    Add reverse_string.java

---
+++ reverse_string.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.println("Reversed string: " + reversed);
+    }
+}
