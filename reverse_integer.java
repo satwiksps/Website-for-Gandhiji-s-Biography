032ac6ffd8f03a91f6e548b0a0f9b98f10531286
Scrodinger's Cat <57804606+satwiksps@users.noreply.github.com>
2020-01-05 16:29:32 +0000

    Add reverse_integer.java

---
+++ reverse_integer.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+class ReverseInteger {
+    public static int reverseNumber(int num) {
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = sc.nextInt();
+        System.out.println("Reversed number: " + reverseNumber(number));
+    }
+}
